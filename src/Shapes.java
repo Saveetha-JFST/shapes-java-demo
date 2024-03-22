ce1b5d2473912eafaa717ae11993b37f593a99ac
sethu-ravichandran <rsethu1997@gmail.com>
2024-03-22 14:14:04 +0530

    Add. Included the logic to calculate the area and perimeter of rectangle.

--- src/Shapes.java
+++ src/Shapes.java
@@ -1,5 +1,38 @@
-class Shapes{
+import java.util.Scanner;
+
+class Shapes {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the length of the rectangle");
+        double length = scanner.nextDouble();
+
+        System.out.println("Enter the breadth of the rectangle");
+        double breadth = scanner.nextDouble();
+
+
+        Rectangle rectangle = new Rectangle(length, breadth);
+
+        System.out.println("Area of the rectangle : " + rectangle.calculateArea() + " sq. units");
+        System.out.println("Perimeter of the rectangle : " + rectangle.calculatePerimeter() + " units");
     }
 }
+
+class Rectangle {
+    private final double length, breadth;
+
+    public Rectangle(double length, double breadth) {
+        this.length = length;
+        this. breadth = breadth;
+    }
+
+    public double calculateArea() {
+        return length * breadth;
+    }
+
+    public double calculatePerimeter() {
+        return 2 * (length + breadth);
+    }
+}
+
+
